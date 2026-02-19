8104c7cd2b84d3f79bd7b19b9c12d62ff7090ce1
KyeongDeok, Baek <blest215@kaist.ac.kr>
2026-02-19 22:37:29 +0900

    Add D20 dataset

--- experiment.py
+++ experiment.py
@@ -164,18 +164,21 @@ if __name__ == "__main__":
         ]}
     ] if args.edge else [
         {"dataset_D5_M0.csv": [
-            # Mid-size <=8b
+            # Small-size <=8b
             Model("ministral-3:8b-instruct-2512-q8_0"),
-            Model("ibm/granite4:tiny-h-q8_0"),
             Model("qwen3:8b-q8_0", reasoning=False),
-            # Small-size <=4b
+            Model("llama3.1:8b-instruct-q8_0"),
+            Model("ibm/granite4:tiny-h-q8_0"),
+            # Tiny-size <=4b
+            Model("qwen3:4b-instruct-2507-q8_0"),
             Model("ministral-3:3b-instruct-2512-q8_0"),
             Model("ibm/granite4:micro-h-q8_0"),
-            Model("qwen3:4b-instruct-2507-q8_0"),
-            # Tiny-size <=1b
-            Model("functiongemma:270m-it-q8_0"),
+            Model("llama3.2:3b-instruct-q8_0"),
+            # Micro-size <=2b
+            Model("tomng/lfm2.5-instruct:1.2b-q8_0"),
             Model("granite4:1b-h-q8_0"),
-            Model("qwen3:1.7b-q8_0", reasoning=False),
+            Model("llama3.2:1b-instruct-q8_0"),
+            Model("functiongemma:270m-it-q8_0"),
         ]},
         # Mutations
         {"dataset_D5_M20.csv": [Model("qwen3:4b-instruct-2507-q8_0")]},
@@ -186,11 +189,13 @@ if __name__ == "__main__":
         # Devices
         {"dataset_D10_M0.csv": [Model("qwen3:4b-instruct-2507-q8_0")]},
         {"dataset_D15_M0.csv": [Model("qwen3:4b-instruct-2507-q8_0")]},
+        {"dataset_D20_M0.csv": [Model("qwen3:4b-instruct-2507-q8_0")]},
         # Settings
         {"dataset_D5_M0.csv": [
             Model("qwen3:0.6b-q8_0", reasoning=False),
             Model("qwen3:0.6b-q4_K_M", reasoning=False),
             Model("qwen3:0.6b-q8_0", reasoning=True),
+            Model("qwen3:1.7b-q8_0", reasoning=False),
             Model("qwen3:1.7b-q4_K_M", reasoning=False),
             Model("qwen3:1.7b-q8_0", reasoning=True),
             Model("qwen3:4b-instruct-2507-q4_K_M"),
@@ -200,15 +205,16 @@ if __name__ == "__main__":
         ]},
         # etc
         {"dataset_D5_M0.csv": [
-            Model("tomng/lfm2.5-instruct:1.2b-q8_0"),
-            Model("hf.co/LiquidAI/LFM2-1.2B-Tool-GGUF:Q8_0"),
-            Model("hf.co/unsloth/SmolLM3-3B-GGUF:Q8_0"),
-            Model("hf.co/LGAI-EXAONE/EXAONE-4.0-1.2B-GGUF:Q8_0"),
             Model("granite4:350m-h-q8_0"),
             Model("rnj-1:8b-instruct-q8_0"),
-            Model("llama3.1:8b-instruct-q8_0"),
-            Model("llama3.2:3b-instruct-q8_0"),
-            Model("llama3.2:1b-instruct-q8_0"),
+            Model("mistral:7b-instruct-v0.3-q8_0"),
+            Model("nemotron-mini:4b-instruct-q8_0"),
+            Model("cogito:8b-v1-preview-llama-q8_0"),
+            Model("cogito:3b-v1-preview-llama-q8_0"),
+            Model("phi4-mini:3.8b-q8_0"),
+            Model("smollm2:1.7b-instruct-q8_0"),
+            Model("qwen3:14b-q8_0", reasoning=False),
+            Model("mistral-nemo:12b-instruct-2407-q8_0"),
         ]}
     ]
 
--- plot.py
+++ plot.py
@@ -11,7 +11,6 @@ from settings import *
 
 plt.rcParams['font.size'] = 13
 
-figure_width = 15
 accuracy_label = "Accuracy Score"
 time_label = "Time (second)"
 dpi = 600
@@ -24,34 +23,37 @@ mode_labels = {"CENTRALIZED": "Baseline", "NATURAL": "LANCE (natural)", "RECRUIT
 stages = {"discovery": "#2ca02c", "plan": "#1f77b4", "control": "#ff7f0e"}
 stage_labels = {"discovery": "Discovery", "plan": "Composition", "control": "Orchestration"}
 
-model_names = [
-    "ministral-3:8b-instruct-2512",
-    "ministral-3:3b-instruct-2512",
-    "functiongemma:270m-it",
-    "granite4:tiny-h",
-    "granite4:7b-a1b-h",
-    "granite4:micro-h",
-    "granite4:3b-h",
-    "granite4:1b-h",
-    "granite4:350m-h",
-    "gpt-oss:20b",
-    "qwen3:0.6b",
-    "qwen3:1.7b",
-    "qwen3:4b-instruct-2507",
-    "qwen3:4b-thinking-2507",
-    "qwen3:8b",
-    "qwen3:30b-a3b-instruct-2507",
-    "phi4-mini:3.8b",
-    "cogito:3b-v1-preview-llama",
-    "smollm2:1.7b-instruct",
-    "rnj-1:8b-instruct",
-    "llama3.1:8b-instruct",
-    "llama3.2:3b-instruct",
-    "llama3.2:1b-instruct",
-]
+model_names = {
+    "lfm2.5-instruct:1.2b": "LFM2.5:1.2B-instruct",
+    "ministral-3:8b-instruct-2512": "Ministral-3:8B-instruct",
+    "ministral-3:3b-instruct-2512": "Ministral-3:3B-instruct",
+    "mistral-nemo:12b-instruct-2407": "Mistral-Nemo:12B-instruct",
+    "functiongemma:270m-it": "FunctionGemma:270M-instruct",
+    "granite4:tiny-h": "Granite4:7B-A1B-h",
+    "granite4:micro-h": "Granite4:3B-h",
+    "granite4:1b-h": "Granite4:1B-h",
+    "granite4:350m-h": "Granite4:350M-h",
+    "gpt-oss:20b": "GPT-OSS:20B",
+    "qwen3:0.6b": "Qwen3:0.6B",
+    "qwen3:1.7b": "Qwen3:1.7B",
+    "qwen3:4b-instruct-2507": "Qwen3:4B-instruct",
+    "qwen3:4b-thinking-2507": "Qwen3:4B-thinking",
+    "qwen3:8b": "Qwen3:8B",
+    "qwen3:14b": "Qwen3:14B",
+    "phi4-mini:3.8b": "phi4-mini:3.8B",
+    "cogito:8b-v1-preview-llama": "cogito:8B-v1-preview-llama",
+    "cogito:3b-v1-preview-llama": "cogito:3B-v1-preview-llama",
+    "smollm2:1.7b-instruct": "smollm2:1.7B-instruct",
+    "nemotron-mini:4b-instruct": "nemotron-mini:4B-instruct",
+    "mistral:7b-instruct-v0.3": "mistral:7B-instruct-v0.3",
+    "rnj-1:8b-instruct": "rnj-1:8B-instruct",
+    "llama3.1:8b-instruct": "llama3.1:8B-instruct",
+    "llama3.2:3b-instruct": "llama3.2:3B-instruct",
+    "llama3.2:1b-instruct": "llama3.2:1B-instruct",
+}
 
 def get_model_name(model_name):
-    for key in model_names:
+    for key, value in model_names.items():
         underbar = key.lower().replace("-", "_").replace(".", "_").replace(":", "_")
         if underbar in model_name:
             return f"{key}{model_name.replace(f'{underbar}_', '-').replace(f'{underbar}', '')}"
@@ -91,8 +93,9 @@ def prepare_plot_data(df):
     for model in all_models:
         metric_data[model] = {}
         for mode in modes.keys():
-            time_total = sum(time_data[model][mode].values())
-            metric_data[model][mode] = (accuracy_data[model][mode] / time_total) if time_total > 0 else 0.0
+            if mode in time_data[model]:
+                time_total = sum(time_data[model][mode].values())
+                metric_data[model][mode] = (accuracy_data[model][mode] / time_total) if time_total > 0 else 0.0
 
     return {"accuracy": accuracy_data, "time": time_data, "metric": metric_data}, all_models
 
@@ -102,10 +105,10 @@ def plot_by_models(path, col_titles, xlabels=None, selected_models=None, name="m
     data, all_models = prepare_plot_data(df)
     
     models = selected_models if selected_models else all_models
-    n_cols = 3
+    n_cols = len(col_titles)
     n_models_per_col = (len(models) + n_cols - 1) // n_cols
     
-    xlabels = [m.capitalize().replace(":", "\n").replace("-q8_0", "").replace("-2512", "").replace("-2507", "").replace("tiny", "7b-a1b").replace("micro", "3b") for m in models] if not xlabels else xlabels
+    xlabels = [model_names[m.replace("-q8_0", "")].replace(":", "\n") for m in models] if not xlabels else xlabels
     
     rows_data = [
         (data["accuracy"], accuracy_label, False, [mode_labels[mode] for mode in modes]),
@@ -113,7 +116,7 @@ def plot_by_models(path, col_titles, xlabels=None, selected_models=None, name="m
         # (data["metric"], f"{accuracy_label} / Time", False, mode_labels),
     ]
 
-    fig, axes = plt.subplots(len(rows_data), n_cols, figsize=(figure_width, 4 * len(rows_data)), squeeze=False, sharey="row", sharex="col")
+    fig, axes = plt.subplots(len(rows_data), n_cols, figsize=(18, 4 * len(rows_data)), squeeze=False, sharey="row", sharex="col")
     
     for row_idx, (row_data, ylabel, is_stacked, legend_labels) in enumerate(rows_data):
         for col_idx in range(n_cols):
@@ -161,17 +164,17 @@ def plot_by_heterogeneity(directory_path, selected_model, devices=None, mutation
     
     if isinstance(devices, list) and isinstance(mutations, int):
         name = "devices"
-        width = 1 + figure_width // 2
+        width = 8.5
         x_pos = np.arange(len(modes)) * 1.5
         configs = [(device, mutations) for device in devices]
         config_labels = [f"{d} devices" for d in devices]
         cmap = plt.cm.YlOrRd
     elif isinstance(devices, int) and isinstance(mutations, list):
         name = "mutations"
-        width = figure_width
+        width = 14.5
         x_pos = np.arange(len(modes)) * 1.5
         configs = [(devices, mutation) for mutation in mutations]
-        config_labels = [f"{m}% mutation" for m in mutations]
+        config_labels = [f"{m}% informal" for m in mutations]
         cmap = plt.cm.YlOrBr
     else:
         return
@@ -220,8 +223,8 @@ def plot_by_hardwares(result_path, selected_models):
             data, all_models = prepare_plot_data(pd.read_csv(file_path))
             times_data[hardware_label] = { model: { mode: data["time"].get(model, {}).get(mode, {}) for mode in data["time"].get(model, {}) } for model in selected_models }
     
-    fig, axes = plt.subplots(1, len(hardwares), figsize=(figure_width, 4), squeeze=False, sharey=None)
-    model_labels = [m.capitalize().replace(":", "\n").replace("-q4_k_m", "\nq4_K_M").replace("-q8_0", "").replace("-2512", "").replace("-2507", "") for m in selected_models]
+    fig, axes = plt.subplots(1, len(hardwares), figsize=(14, 4), squeeze=False, sharey=None)
+    model_labels = [model_names[m.replace("-q4_K_M", "")].replace(":", "\n") + "\nq4_K_M" for m in selected_models]
         
     for col_idx, hardware in enumerate(hardwares.keys()):        
         x = np.arange(len(selected_models))
@@ -277,9 +280,7 @@ def plot_failure(result_path, selected_models=None):
     else:
         display_messages = sorted_messages
     
-    labels = [msg for msg, _ in display_messages]
-    sizes = [count for _, count in display_messages]
-    total = sum(sizes)
+    total = sum([count for _, count in display_messages])
 
     for failure, count in display_messages:
         print(f"{failure} & {(count / total)*100:.2f}\\% &  \\\\")
@@ -300,20 +301,25 @@ if __name__ == "__main__":
 
     main_models = [
         "ministral-3:8b-instruct-2512-q8_0",
-        "granite4:tiny-h-q8_0",
         "qwen3:8b-q8_0",
+        "llama3.1:8b-instruct-q8_0",
+        "granite4:tiny-h-q8_0",
+
+        "qwen3:4b-instruct-2507-q8_0",
         "ministral-3:3b-instruct-2512-q8_0",
         "granite4:micro-h-q8_0",
-        "qwen3:4b-instruct-2507-q8_0",
-        "functiongemma:270m-it-q8_0",
+        "llama3.2:3b-instruct-q8_0",
+        
+        "lfm2.5-instruct:1.2b-q8_0",
         "granite4:1b-h-q8_0",
-        "qwen3:1.7b-q8_0",
+        "llama3.2:1b-instruct-q8_0",
+        "functiongemma:270m-it-q8_0",
     ]
 
     plot_failure(f"{RESULT_DIR}/{code}/result_D5_M0.csv", selected_models=main_models)
 
-    plot_by_models(f"{RESULT_DIR}/{code}/result_D5_M0.csv", col_titles=["Small-size (<=8b)", "Tiny-size (<=4b)", "Micro-size (<=2b)"], selected_models=main_models, name="models")
-    plot_by_models(f"{RESULT_DIR}/{code}/result_D5_M0.csv", col_titles=["Small-size (qwen3:8b)", "Tiny-size (qwen3:4b-instruct)", "Micro-size (qwen3:1.7b)"], selected_models=[
+    plot_by_models(f"{RESULT_DIR}/{code}/result_D5_M0.csv", col_titles=["Small (<=8B)", "Tiny (<=4B)", "Micro (<=2B)"], selected_models=main_models, name="models")
+    plot_by_models(f"{RESULT_DIR}/{code}/result_D5_M0.csv", col_titles=["Qwen3:8B", "Qwen3:4B-instruct", "Qwen3:1.7B", "Qwen3:0.6B"], selected_models=[
         "qwen3:8b-q8_0_reasoning",
         "qwen3:8b-q8_0",
         "qwen3:8b-q4_K_M",
@@ -323,9 +329,12 @@ if __name__ == "__main__":
         "qwen3:1.7b-q8_0_reasoning",
         "qwen3:1.7b-q8_0",
         "qwen3:1.7b-q4_K_M",
-    ], name="settings", xlabels=["q8_0 (reasoning)", "q8_0", "q4_K_M"] * 3)
+        "qwen3:0.6b-q8_0_reasoning",
+        "qwen3:0.6b-q8_0",
+        "qwen3:0.6b-q4_K_M",
+    ], name="settings", xlabels=["q8_0\n(reasoning)", "q8_0", "q4_K_M"] * 4)
     
     plot_by_heterogeneity(f"{RESULT_DIR}/{code}", "qwen3:4b-instruct-2507-q8_0", devices=5, mutations=[0, 20, 40, 60, 80, 100])
-    plot_by_heterogeneity(f"{RESULT_DIR}/{code}", "qwen3:4b-instruct-2507-q8_0", devices=[5, 10, 15], mutations=0)
+    plot_by_heterogeneity(f"{RESULT_DIR}/{code}", "qwen3:4b-instruct-2507-q8_0", devices=[5, 10, 15, 20], mutations=0)
     
     plot_by_hardwares(f"{RESULT_DIR}/{code}/result_D5_M0.csv", ["qwen3:8b-q4_K_M", "qwen3:4b-instruct-2507-q4_K_M", "qwen3:1.7b-q4_K_M", "qwen3:0.6b-q4_K_M"])
